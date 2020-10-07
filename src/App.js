6b78c1b1240089bb2585c6de9fee8dfebe289863
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-10-07 12:16:30 -0400

    made access to each user database

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Auth from "./components/Auth/Auth";
 import Main from "./components/Database/Main";
+import database from "./core/firebase";
 
 const App = () => {
   const [user, setUser] = useState("");
 
   const handleSetUser = (user) => {
+    // createCollection(user.uid);
     setUser(user);
   };
+
+  // const createCollection = (id) => {
+  //   // database.collection("what").doc(id).set();
+  //   // console.log(
+  //   //   database
+  //   //     .collection("users")
+  //   //     .doc("fOAV5T6d8lMDhOMsE9oZ0v6uIpf2")
+  //   //     .onSnapshot((doc) => {
+  //   //       console.log(doc.data().test);
+  //   //     })
+  //   // );
+
+  //   // const listTest = ["1", "2"];
+  //   // const InfoTest = { 1: "wefwef", 2: "wefwg" };
+
+  //   console.log(
+  //     database
+  //       .collection("users")
+  //       .doc(id)
+  //       .get()
+  //       .then((doc) => doc.size)
+  //   );
+  //   console.log(database.collection("users").doc("111").get());
+
+  //   database
+  //     .collection("users")
+  //     .doc(id)
+  //     .collection("userData")
+  //     .doc("fullJobsInfo")
+  //     .set({});
+  //   database
+  //     .collection("users")
+  //     .doc(id)
+  //     .collection("userData")
+  //     .doc("listOfJobs")
+  //     .set({});
+  // };
+
   return (
     <div className="App">
-      {user ? <Main /> : <Auth handleSetUser={handleSetUser} />}
+      {user ? <Main user={user} /> : <Auth handleSetUser={handleSetUser} />}
 
       {/* */}
+      {/* <Auth handleSetUser={handleSetUser} /> */}
     </div>
   );
 };
