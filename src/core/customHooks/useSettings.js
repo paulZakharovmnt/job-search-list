0d0ce8fd64f750fb6d31bf2e9783386b388a961e
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-11-03 15:14:48 -0500

    add set cities function

--- src/core/customHooks/useSettings.js
+++ src/core/customHooks/useSettings.js
@@ -16,53 +16,53 @@ const useSettings = (user) => {
   ]);
 
   const [cityOfCompanyWhereApplied, setCityOfCompanyWhereApplied] = useState([
-    "Montreal",
-    "Toronto",
-    "Vancouver",
+    // "Montreal",
+    // "Toronto",
+    // "Vancouver",
   ]);
 
-  // useEffect(() => {
-  //   getCitiesListFromDatabse();
-  //   console.log("get sities");
-  // }, []);
+  useEffect(() => {
+    if (cityOfCompanyWhereApplied.length > 0) {
+      console.log("set cities");
+      setNewListOfCitiestoFireBase();
+    }
+  }, [cityOfCompanyWhereApplied]);
 
-  // useEffect(() => {
-  //   if (cityOfCompanyWhereApplied.length > 0) {
-  //     console.log("set cities");
-  //     setNewListOfCitiestoFireBase();
-  //   }
-  // }, [cityOfCompanyWhereApplied]);
+  useEffect(() => {
+    getCitiesListFromDatabse();
+    console.log("get sities");
+  }, []);
 
-  // const getCitiesListFromDatabse = () => {
-  //   database
-  //     .collection("users")
-  //     .doc(user.uid)
-  //     .collection("settings")
-  //     .doc("cities")
-  //     .onSnapshot((doc) => {
-  //       setCityOfCompanyWhereApplied(doc.data().cityOfCompanyWhereApplied);
-  //     });
-  // };
+  const getCitiesListFromDatabse = () => {
+    database
+      .collection("users")
+      .doc(user.uid)
+      .collection("settings")
+      .doc("cities")
+      .onSnapshot((doc) => {
+        setCityOfCompanyWhereApplied(doc.data().cityOfCompanyWhereApplied);
+      });
+  };
 
-  // const setNewListOfCitiestoFireBase = () => {
-  //   database
-  //     .collection("users")
-  //     .doc(user.uid)
-  //     .collection("userData")
-  //     .doc("listOfJobs")
-  //     .set({ cityOfCompanyWhereApplied });
-  // };
+  const setNewListOfCitiestoFireBase = () => {
+    database
+      .collection("users")
+      .doc(user.uid)
+      .collection("settings")
+      .doc("cities")
+      .set({ cityOfCompanyWhereApplied });
+  };
 
-  // const handleAddNewItemToList = (item) => {
-  //   console.log(item);
-  //   setCityOfCompanyWhereApplied([...cityOfCompanyWhereApplied, item]);
-  // };
+  const handleAddNewItemToList = (item) => {
+    console.log(item);
+    setCityOfCompanyWhereApplied([...cityOfCompanyWhereApplied, item]);
+  };
   return [
     {
       sourcesListOfVacancy,
       resultsListOfInterviews,
       cityOfCompanyWhereApplied,
-      // handleAddNewItemToList,
+      handleAddNewItemToList,
     },
   ];
 };
