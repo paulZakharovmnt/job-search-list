e1a0906ff7f24c1977a5f77224c206748b7fa0d9
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-11-03 11:46:32 -0500

    Updates

---
+++ precache-manifest.d7f9c5666bb4480e7b52166b4fc80924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5069ce652daf24e361a9dcef3e015411",
+    "url": "/job-search-list/index.html"
+  },
+  {
+    "revision": "9280c7f001c17d261859",
+    "url": "/job-search-list/static/css/main.a0465097.chunk.css"
+  },
+  {
+    "revision": "b9206c505b384a04495f",
+    "url": "/job-search-list/static/js/2.bd5e6c90.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/job-search-list/static/js/2.bd5e6c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9280c7f001c17d261859",
+    "url": "/job-search-list/static/js/main.42607b60.chunk.js"
+  },
+  {
+    "revision": "5bf4de17c369565b9cbb",
+    "url": "/job-search-list/static/js/runtime-main.8b885089.js"
+  }
+]);
