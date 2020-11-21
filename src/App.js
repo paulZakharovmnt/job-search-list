b06f66eaf086d014c62c2251c5bae732be6af4dc
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-11-21 11:21:38 -0500

    Wrap components in settings context

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Auth from "./components/Auth/Auth";
 import Main from "./components/Database/Main";
 import AuthContext from "./context/auth-context/auth-context";
 import ApplicationsState from "./context/applications-context/ApplicationsState";
+import SettingsState from "./context/settings-context/SettingsState";
 
 const App = () => {
   const { user } = useContext(AuthContext);
@@ -16,7 +17,9 @@ const App = () => {
         <Auth />
       ) : (
         <ApplicationsState>
-          <Main user={user} />
+          <SettingsState>
+            <Main user={user} />
+          </SettingsState>
         </ApplicationsState>
       )}
     </div>
