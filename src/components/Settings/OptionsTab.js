f94f0a47cad39e4735220bc70fe28caa2c7daf74
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-11-21 11:31:20 -0500

    Change structure, split to different componens

--- src/components/Settings/OptionsTab.js
+++ src/components/Settings/OptionsTab.js
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect } from "react";
-import AddInput from "./AddInput";
 import settingsContext from "../../context/settings-context/settings-context";
 import authContext from "../../context/auth-context/auth-context";
 import setUpdatedListOfSourcesToFB from "../../core/setToFBFunctions/setUpdatedListOfSourcesToFB";
 import setUpdatedListOfResultsToFB from "../../core/setToFBFunctions/setUpdatedListOfResultsToFB";
 import setUpdatedListOfCitiesToFB from "../../core/setToFBFunctions/setUpdatedListOfCitiesToFB";
 import "./OptionsTab.css";
+import ListOfCities from "./OptionTabs/ListOfCities";
+import ListOfSources from "./OptionTabs/ListOfSources";
+import ListOfResults from "./OptionTabs/ListOfResults";
 
 const OptionsTab = ({ optionTabToRender }) => {
   const {
@@ -45,62 +47,31 @@ const OptionsTab = ({ optionTabToRender }) => {
 
   if (optionTabToRender === "Cities") {
     return (
-      <div className="selector-contaner2">
-        <h4>Delete or Add new city in the List:</h4>
-        <ul className="list-of-items">
-          {listOfCities.map((city) => (
-            <div key={city} className="list-contaniner">
-              <li>{city}</li>
-              <button onClick={() => deleteCity(city)}>Delete</button>
-              <i
-                onClick={() => deleteCity(city)}
-                className="delete-item-btn fas fa-trash-alt"
-              ></i>
-            </div>
-          ))}
-        </ul>
-        <AddInput addNewOptionToList={addCityToList} />
-      </div>
+      <ListOfCities
+        listOfCities={listOfCities}
+        deleteCity={deleteCity}
+        addCityToList={addCityToList}
+      />
     );
   }
 
   if (optionTabToRender === "Sources") {
     return (
-      <div className="selector-contaner2">
-        <h4>Delete or Add new source in the List:</h4>
-        <ul className="list-of-items">
-          {listOfSources.map((source) => (
-            <div key={source} className="list-contaniner">
-              <li>{source}</li>
-              <i
-                onClick={() => deleteSource(source)}
-                className="delete-item-btn fas fa-trash-alt"
-              ></i>
-            </div>
-          ))}
-        </ul>
-        <AddInput addNewOptionToList={addSourceToList} />
-      </div>
+      <ListOfSources
+        listOfSources={listOfSources}
+        addSourceToList={addSourceToList}
+        deleteSource={deleteSource}
+      />
     );
   }
 
   if (optionTabToRender === "Results") {
     return (
-      <div className="selector-contaner2">
-        <h4>Delete or Add new result in the List:</h4>
-        <ul className="list-of-items">
-          {listOfResults.map((result) => (
-            <div key={result} className="list-contaniner">
-              <li>{result}</li>
-              <i
-                onClick={() => deleteResult(result)}
-                className="delete-item-btn fas fa-trash-alt"
-              ></i>
-            </div>
-          ))}
-        </ul>
-        <AddInput addNewOptionToList={addResultToList} />
-      </div>
+      <ListOfResults
+        listOfResults={listOfResults}
+        deleteResult={deleteResult}
+        addResultToList={addResultToList}
+      />
     );
   }
 };
