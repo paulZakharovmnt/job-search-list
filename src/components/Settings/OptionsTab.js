3cbfbf05ffb8c61357220a436b3e61a8eab4242e
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-11-21 11:21:12 -0500

    Add settings context to component

--- src/components/Settings/OptionsTab.js
+++ src/components/Settings/OptionsTab.js
@@ -1,39 +1,65 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import AddInput from "./AddInput";
+import settingsContext from "../../context/settings-context/settings-context";
+import authContext from "../../context/auth-context/auth-context";
+import setUpdatedListOfSourcesToFB from "../../core/setToFBFunctions/setUpdatedListOfSourcesToFB";
+import setUpdatedListOfResultsToFB from "../../core/setToFBFunctions/setUpdatedListOfResultsToFB";
+import setUpdatedListOfCitiesToFB from "../../core/setToFBFunctions/setUpdatedListOfCitiesToFB";
 import "./OptionsTab.css";
 
-const OptionsTab = ({
-  optionTabToRender,
-  user,
-  listOfSourcesFromSelectorMenu,
-  listOfResultsFromSelectorMenu,
-  listOfCitiesFromSelectorMenu,
-  handleAddNewCityToListSubmit,
-  handleAddNewResultToListSubmit,
-  handleAddNewSourceToListSubmit,
-  handleDeleteCityFromList,
-  handleDeleteResultFromList,
-  handleDeleteSourceFromList,
-}) => {
+const OptionsTab = ({ optionTabToRender }) => {
+  const {
+    listOfCities,
+    listOfResults,
+    listOfSources,
+    addCityToList,
+    addResultToList,
+    addSourceToList,
+    deleteCity,
+    deleteResult,
+    deleteSource,
+  } = useContext(settingsContext);
+
+  const { user } = useContext(authContext);
+
+  useEffect(() => {
+    if (listOfSources.length < 1) {
+      return;
+    }
+    setUpdatedListOfSourcesToFB(user, listOfSources);
+  }, [listOfSources, user]);
+
+  useEffect(() => {
+    if (listOfResults.length < 1) {
+      return;
+    }
+    setUpdatedListOfResultsToFB(user, listOfResults);
+  }, [listOfResults, user]);
+
+  useEffect(() => {
+    if (listOfCities.length < 1) {
+      return;
+    }
+    setUpdatedListOfCitiesToFB(user, listOfCities);
+  }, [listOfCities, user]);
+
   if (optionTabToRender === "Cities") {
     return (
       <div className="selector-contaner2">
         <h4>Delete or Add new city in the List:</h4>
         <ul className="list-of-items">
-          {listOfCitiesFromSelectorMenu.map((city) => (
+          {listOfCities.map((city) => (
             <div key={city} className="list-contaniner">
               <li>{city}</li>
+              <button onClick={() => deleteCity(city)}>Delete</button>
               <i
-                onClick={() => handleDeleteCityFromList(city)}
+                onClick={() => deleteCity(city)}
                 className="delete-item-btn fas fa-trash-alt"
               ></i>
             </div>
           ))}
         </ul>
-        <AddInput
-          addNewOptionToList={handleAddNewCityToListSubmit}
-          user={user}
-        />
+        <AddInput addNewOptionToList={addCityToList} />
       </div>
     );
   }
@@ -43,20 +69,17 @@ const OptionsTab = ({
       <div className="selector-contaner2">
         <h4>Delete or Add new source in the List:</h4>
         <ul className="list-of-items">
-          {listOfSourcesFromSelectorMenu.map((source) => (
+          {listOfSources.map((source) => (
             <div key={source} className="list-contaniner">
               <li>{source}</li>
               <i
-                onClick={() => handleDeleteSourceFromList(source)}
+                onClick={() => deleteSource(source)}
                 className="delete-item-btn fas fa-trash-alt"
               ></i>
             </div>
           ))}
         </ul>
-        <AddInput
-          addNewOptionToList={handleAddNewSourceToListSubmit}
-          user={user}
-        />
+        <AddInput addNewOptionToList={addSourceToList} />
       </div>
     );
   }
@@ -66,20 +89,17 @@ const OptionsTab = ({
       <div className="selector-contaner2">
         <h4>Delete or Add new result in the List:</h4>
         <ul className="list-of-items">
-          {listOfResultsFromSelectorMenu.map((result) => (
+          {listOfResults.map((result) => (
             <div key={result} className="list-contaniner">
               <li>{result}</li>
               <i
-                onClick={() => handleDeleteResultFromList(result)}
+                onClick={() => deleteResult(result)}
                 className="delete-item-btn fas fa-trash-alt"
               ></i>
             </div>
           ))}
         </ul>
-        <AddInput
-          addNewOptionToList={handleAddNewResultToListSubmit}
-          user={user}
-        />
+        <AddInput addNewOptionToList={addResultToList} />
       </div>
     );
   }
