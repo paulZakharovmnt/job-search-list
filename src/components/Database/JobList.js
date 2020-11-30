b7be22ced9fb245579757b3a0dc3b73cf7095828
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-11-30 12:46:17 -0500

    add classes

--- src/components/Database/JobList.js
+++ src/components/Database/JobList.js
@@ -17,38 +17,40 @@ const JobList = ({
     return company.toLowerCase().includes(searchInputValue.toLowerCase());
   });
   return (
-    <ul className="job-list">
-      {!showFrenchLanguage ? (
-        <li className="job-header">
-          <p>Company Name</p>
-          <p>City</p>
-          <p>Date</p>
-          <p>Source</p>
-          <p>Result</p>
-          <p>Edit/Delete</p>
-        </li>
-      ) : (
-        <li className="job-header">
-          <p>Nom de la compagnie</p>
-          <p>Ville</p>
-          <p>Date</p>
-          <p>La source</p>
-          <p>Résultat</p>
-          <p>Modifier/Supprimer</p>
-        </li>
-      )}
-      {filteredApplications.map((company) => {
-        return (
-          <Job
-            company={company}
-            applicationsById={applicationsById}
-            key={company}
-            handleDeleteApplicationClick={handleDeleteApplicationClick}
-            handleOpenEditJobModalClick={handleOpenEditJobModalClick}
-          />
-        );
-      })}
-    </ul>
+    <div className="job-list-container">
+      <div className="job-list">
+        {!showFrenchLanguage ? (
+          <div className="job-header">
+            <p className="job-header-company-name">Company Name</p>
+            <p className="job-header-city">City</p>
+            <p className="job-header-date">Date</p>
+            <p className="job-header-source">Source</p>
+            <p className="job-header-result">Result</p>
+            <p className="job-header-edit">Edit/Delete</p>
+          </div>
+        ) : (
+          <div className="job-header">
+            <p>Nom de la compagnie</p>
+            <p>Ville</p>
+            <p>Date</p>
+            <p>La source</p>
+            <p>Résultat</p>
+            <p>Modifier/Supprimer</p>
+          </div>
+        )}
+        {filteredApplications.map((company) => {
+          return (
+            <Job
+              company={company}
+              applicationsById={applicationsById}
+              key={company}
+              handleDeleteApplicationClick={handleDeleteApplicationClick}
+              handleOpenEditJobModalClick={handleOpenEditJobModalClick}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
