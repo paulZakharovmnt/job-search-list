4e894f0892b656cd5c668740fbbfd29224bbfa5a
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-11-30 12:45:24 -0500

    Put different lang text in sepparate file

---
+++ src/core/englishFrenchTexts.js
@@ -0,0 +1,18 @@
+export const naviButtomsText = {
+  addNewJob: {
+    english: "Add New Job",
+    french: "Ajouter un nouveau travail",
+  },
+  listOfJobs: {
+    english: "List of Jobs",
+    french: "Liste des emplois",
+  },
+  settings: {
+    english: "Settings",
+    french: "Réglages",
+  },
+  logout: {
+    english: "Logout",
+    french: "Se déconnecter",
+  },
+};
